7fe46183d23f2eab9e421a0368d81ad42b544c5d
DeceulaerAdrien <adriendeceulaer@gmail.com>
2024-04-10 16:11:22 +0200

    regex init

---
+++ Python/03.Regex.py
@@ -0,0 +1,30 @@
+import re
+# 1. Create a regex that finds integers without size limit.
+s = "sssgdds8sfsfs"
+pattern = "\d"
+
+print(re.findall(pattern,s))
+
+# 2. Create a regex that finds negative integers without size limit.
+s = "sssgdds-8sfsfs"
+pattern = "-\d"
+
+print(re.findall(pattern,s))
+
+# 3. Create a regex that finds (positive or negative) integers without size limit.
+s = "sssgdds-8s8fsfs"
+pattern = "-?\d"
+
+print(re.findall(pattern,s))
+
+# 4. Capture all the numbers of the following sentence :
+text = "21 scouts and 3 tanks fought against 4,003 protestors, so the manager was not 100.00% happy."
+pattern = "\d+(?:,\d+)*(?:\.\d+)?"
+
+print(re.findall(pattern,text))
+
+# 5. Find all words that end with 'ly'.
+text = "He had prudently disguised himself but was quickly captured by the police."
+pattern = "\b\w+ly\b"
+
+print(re.findall(text,pattern))
