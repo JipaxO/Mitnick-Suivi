cb4a52e262e2d49190c97cf578cfaded277ab4ec
Deceulaer Adrien <adriendeceulaer@gmail.com>
2024-04-11 16:37:31 +0200

    scraping-2

--- Python/05.scraping.py
+++
@@ -1,8 +0,0 @@
-from lxml import etree
-
-filename = "./data/scraping/data.xml"
-
-tree = etree.parse(filename)
-
-for user in tree.xpath("/users/user[job='Veterinary']/name"):
-    print(user.text)
