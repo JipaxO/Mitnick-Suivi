a06923e60fc390ac77dd9af3e79b9d6003c07232
DeceulaerAdrien <adriendeceulaer@gmail.com>
2024-04-12 13:30:36 +0200

    allociné scrapping

--- Python/05.scraping.py
+++ Python/05.scraping.py
@@ -1,5 +1,7 @@
-from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import time
+from bs4 import BeautifulSoup
 
 url = "http://www.allocine.fr/"
 r = requests.get(url)
@@ -8,22 +10,37 @@ soup = BeautifulSoup(r.content, "lxml")
 
 # for p in soup.find_all("a"):
 #     print(p.text)
-
+title = []
+synopsis = []
 links = []
-for elem in soup.find_all("a", attrs={"class": "meta-title meta-title-link"}):
+target_syp = None
+
+for elem in soup.find_all("a", class_="meta-title meta-title-link"):
     links.append(elem.get("href"))
 
 movie_links = ["http://www.allocine.fr" + elem for elem in links if "film" in elem]
 
-url = movie_links[0]
-r = requests.get(url)
-print(url, r.status_code)
-soup = BeautifulSoup(r.content, "lxml")
+for links in movie_links:
 
-for elem in soup.find_all("div", attrs={"class": "titlebar-title titlebar-title-xl"}):
-    # Just like that
-    print(elem.text)
+    r = requests.get(links)
+    soup = BeautifulSoup(r.content, "lxml")
 
-for elem in soup.find_all("div", attrs={"class": "content-txt"}):
-    # Just like that
-    print(elem.text)
+    for elem in soup.find_all("div", class_="titlebar-title titlebar-title-xl"):
+        title.append(elem.text)
+
+    for elem in soup.find_all("div", class_="content-txt"):  # ------------------→ J'ai galéré comme pas permis
+        if elem.find("p", class_="bo-p"):
+            target_syp = elem
+
+    if target_syp:
+        synopsis.append(target_syp.text)
+
+print("Total titles:", len(title))
+print("Total synopsis:", len(synopsis))
+print("Total movie links:", len(movie_links))
+
+df = pd.DataFrame({"Title": title})
+df["Synopsis"] = synopsis
+df["Links"] = movie_links
+
+df.to_csv("./data/scraping/allo_cine.csv", index=False)
