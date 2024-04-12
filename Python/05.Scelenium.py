208cc79a04aa33dad7b0e8f9f826a0f2a534236a
DeceulaerAdrien <adriendeceulaer@gmail.com>
2024-04-12 13:56:01 +0200

    allociné scrapping name

---
+++ Python/05.Scelenium.py
@@ -0,0 +1,18 @@
+import os
+import re
+import collections
+import bs4
+import requests
+import numpy as np
+import pandas as pd
+import json
+import lxml.html
+import time
+import random
+import logging
+from bs4 import BeautifulSoup
+from random import randint
+from time import gmtime, strftime
+from tabulate import tabulate
+
+
