060f726ef7cf2c9d6a3ff688ff0e20f45f29495f
DeceulaerAdrien <adriendeceulaer@gmail.com>
2024-04-15 16:09:55 +0200

    threading exo

---
+++ Python/06.Thrading.py
@@ -0,0 +1,10 @@
+from threading import Thread
+
+
+class Sheakspears(Thread):
+    def __init__(self, sonnets):
+        Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        sonnets = []
